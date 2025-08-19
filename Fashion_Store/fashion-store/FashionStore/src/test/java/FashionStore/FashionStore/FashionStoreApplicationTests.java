415eeba1e7dee349d68a0030af0b3fa87c29eb1d
Praveen <149309291+praveenpramodh2002@users.noreply.github.com>
2025-08-19 18:29:05 +0530

    backend

---
+++ Fashion_Store/fashion-store/FashionStore/src/test/java/FashionStore/FashionStore/FashionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package FashionStore.FashionStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
